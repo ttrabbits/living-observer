cc6c807df6d219387df1c12bc51c979d2d0d37b8
ttrabbits <terabbits2@gmail.com>
2021-02-14 02:47:01 +0900

    test movement

--- src/Code.ts
+++ src/Code.ts
@@ -3,25 +3,27 @@ import { getLastEvents, getSettings } from './remo'
 const spreadsheetId = '1Dxzqk2o11eLu7WwikpD7GOwaQSQtavkYfyU8XOmuNqI'
 
 function main() {
-  const date = Utilities.formatDate(
-    new Date(),
-    'Asia/Tokyo',
-    'yyyy-MM-dd HH:mm:ss'
-  )
+  const time = formatTime(new Date())
   const events = getLastEvents()
   const settings = getSettings()
+  const movedTime = formatTime(new Date(events.mo.created_at))
 
   const data = [
-    date,
+    time,
     events.te.val,
     events.hu.val,
     events.il.val,
     settings.light.power === 'on' ? '1' : '0',
-    settings.light.brightness,
     settings.aircon.button === 'power-off' ? '0' : '1',
     settings.aircon.temp,
+    events.mo.val,
+    movedTime,
   ]
 
   const sheet = SpreadsheetApp.openById(spreadsheetId).getActiveSheet()
   sheet.appendRow(data)
+}
+
+function formatTime(date: Date) {
+  return Utilities.formatDate(date, 'Asia/Tokyo', 'yyyy-MM-dd HH:mm:ss')
 }
